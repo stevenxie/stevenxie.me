0e02c256f8e550cb1d847c7a5a704175e99ca7bd
Steven Xie <dev@stevenxie.me>
2019-03-14 08:22:12 -0400

    Made CZ's 'F' more surreal

--- src/views/CZ.vue
+++ src/views/CZ.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cz flex col">
-    <div id="f" :class="{ started: started }" />
-    <button v-if="!started" @click="start">
+    <div id="f" :class="{ started: startTime }" />
+    <button v-if="!startTime" @click="start">
       clinical depression
     </button>
   </div>
@@ -12,9 +12,10 @@ function buildMaterial() {
   const { LiquidDistortMaterial } = window.Blotter;
   const mat = new LiquidDistortMaterial();
 
+  // Configure material uniforms.
   const { uniforms } = mat;
   uniforms.uSpeed.value = 0.255555;
-  uniforms.uVolatility.value = 0.07;
+  uniforms.uVolatility.value = 0.05;
   uniforms.uSeed.value = 1.15;
 
   return mat;
@@ -29,10 +30,11 @@ export default {
       { src: "https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/materials/liquidDistortMaterial.min.js" },
     ]
   },
-  data: () => ({ started: false }),
+  data: () => ({ startTime: null, material: null }),
   methods: {
+    /** Initializes the 'F', and starts music. */
     start() {
-      this.started = true;
+      this.startTime = new Date();
 
       // Build and insert animated 'F'.
       const { Text } = window.Blotter;
@@ -41,14 +43,26 @@ export default {
         size: 150,
         fill: "rgb(218, 218, 218)",
       });
-      const material = buildMaterial();
-      const blotter = new window.Blotter(material, { texts: text });
+      this.material = buildMaterial();
+      const blotter = new window.Blotter(this.material, { texts: text });
       const scope = blotter.forText(text);
       scope.appendTo(document.getElementById("f"));
 
       // Start the music.
       const audio = new Audio("./cz-titanic.mp3");
       audio.play();
+
+      // Begin surrealification process after 5 second.
+      setTimeout(this.surrealify, 5000);
+    },
+
+    /** Makes the 'F' more... surreal-lookin'. */
+    surrealify() {
+      const { startTime, material } = this;
+      const secondsSinceStart = parseInt(new Date() - startTime) / 1000;
+      if (secondsSinceStart > 30) return; // stop 30 seconds after start
+      material.uniforms.uVolatility.value += 0.01;
+      setTimeout(this.surrealify, 1000); // repeat
     },
   },
 };
@@ -79,7 +93,7 @@ export default {
       90% { opacity: 1; }
       100% { opacity: 0; }
     }
-    animation: transition 31s;
+    animation: transition 32s;
   }
 }
 
