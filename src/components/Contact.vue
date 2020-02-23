691ee33997fb9046aeb0abf81c48e70078be1b6d
Steven Xie <dev@stevenxie.me>
2020-02-23 00:33:08 -0500

    Use new 'memoji' avatar

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="contact flex" id="contact">
-    <img class="avatar" src="@/assets/ori.svg" alt="Avatar" />
+    <img class="avatar" src="@/assets/memoji.png" alt="Avatar" />
     <h2 class="chat">Wanna chat?</h2>
     <p class="message">Shoot me a message at</p>
     <span class="email-wrapper" :class="{ error }">
